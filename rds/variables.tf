2157234d0bfef54476574d3fa8ca8aa21a79935b
Chris Hickey <chickey@mktn-chickey.lan>
2023-03-10 10:49:54 -0700

    changing database variables and adding new stuff

---
+++ rds/variables.tf
@@ -0,0 +1,118 @@
+ variable "allocated_storage" {
+    description = 
+    default = 
+ }
+
+variable "availability_zone" {
+    description = 
+    default = 
+}
+
+variable "backup_retention_period" {
+    description = 
+    default = 
+}
+
+variable "backup_retention_period" {
+    
+}
+
+variable "backup_window" {
+
+}
+
+variable "backup_window" {
+
+}
+
+variable "ca_cert_identifier" {
+
+}
+
+variable "db_name" {
+
+}
+
+variable "db_subnet_group_name" {
+
+}
+
+variable "delete_automated_backups" {
+
+}
+
+variable "enabled_cloudwatch_logs_exports" {
+
+}
+
+variable "engine" {
+
+}
+
+variable "engine_version" {
+
+}
+
+variable "identifier" {
+
+}
+
+variable "instance_class" {
+
+}
+
+variable "kms_key_id" {
+
+}
+
+variable "license_model" {
+
+}
+
+variable "maintence_window" {
+
+}
+
+variable "monitoring_interval" {
+
+}
+
+variable "monitoring_role_arn" {
+
+}
+
+variable "option_group_name" {
+
+}
+
+variable "parameter_group_name" {
+
+}
+
+variable "performance_insights_enabled" {
+
+}
+
+variable "performance_insights_kms_key_id" {
+
+}
+
+variable "performance_retention_period" {
+
+}
+
+variable "port" {
+
+}
+
+variable "publicly_accessible" {
+
+}
+
+variable "skip_final_snapshot" {}
+
+variable "storage_encrypted" {}
+
+variable "username" {}
+
+variable "vpc_security_group_ids" {}
